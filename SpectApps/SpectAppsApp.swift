a034216e301a80815036f1a33bf532c54fc0b04c
MalikEmiroglu <malikemiroglu92@gmail.com>
2025-07-18 17:07:04 +0300

    Initial Commit

---
+++ SpectApps/SpectAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectAppsApp.swift
+//  SpectApps
+//
+//  Created by Malik on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpectAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
